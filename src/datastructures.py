d74937b6bc23a487fbcbc24028e7e643d64a3808
Jonathan Villalobos Mora <hackertristania@gmail.com>
2021-02-24 00:02:05 +0000

    members @datastruc path

--- src/datastructures.py
+++ src/datastructures.py
@@ -13,13 +13,37 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [{
+            "id": self._generateId(),
+            "first_name": "John",
+            "last_name": "Jackson",
+            "age": 33,
+            "lucky_numbers":[7,13,22]
+        },
+        {
+            "id": self._generateId(),
+            "first_name": "Jane",
+            "last_name": "Jackson",
+            "age": 35,
+            "lucky_numbers":[10,14,3]
+        },
+        {
+            "id": self._generateId(),
+            "first_name": "John",
+            "last_name": "Jackson",
+            "age": 5,
+            "lucky_numbers":[1]
+        },
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
+    
     def _generateId(self):
         return randint(0, 99999999)
 
+    @app.route('/members', methods=['POST'])    
     def add_member(self, member):
+        self._members.append()
         # fill this method and update the return
         pass
 
